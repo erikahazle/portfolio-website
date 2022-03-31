7950fda261e61886fd4f5132c5b6e1cd42234a6d
erikajon <hello@jonika.io>
2022-03-31 20:54:50 +0100

    Hello section fixed

--- containers/Home/Sections/Hello/index.tsx
+++ containers/Home/Sections/Hello/index.tsx
@@ -4,11 +4,11 @@ export const HelloSection = () => (
     <div className='max-w-screen-lg mx-auto py-16'>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex py-8 md:flex-row flex-col items-center"> 
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 z-10 m-auto flex justify-center">
-            <img className="object-cover object-center max-h-96" alt="hero" src="/images/hello.jpg" />
+          <div className="z-10 m-auto flex justify-center">
+            <img className="object-cover object-center max-h-96 m-8 mb-16" alt="hero" src="/images/hello.jpg" />
           </div>
-          <div className="lg:flex-grow md:w-1/2 px-5 pr-16  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 pb-8   text-gray-900 font-montserrat font-bold">
+          <div className="lg:flex-grow md:w-1/2 pl-16 px-5 pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 pb-8 text-gray-900 font-montserrat font-bold">
               Hi, I’m Erika. Nice to meet you!
             </h1> 
             <p className="mb-8 leading-relaxed font-montserrat">
