69af9f88de895edc09f4302dfc6d1eaa6436b645
erikajon <hello@jonika.io>
2022-04-03 12:23:30 +0100

    Layout fixes

--- containers/Home/Sections/Launch/index.tsx
+++ containers/Home/Sections/Launch/index.tsx
@@ -3,7 +3,7 @@ export const LaunchSection = () => (
     <div className='max-w-screen-lg mx-auto py-16'>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex py-8 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 px-5 pr-16  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="lg:flex-grow md:w-1/2 px-5 pr-5 md:pr-16  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 pb-8   text-gray-900 font-montserrat font-bold">
               Let’s get your idea from notepad to launchpad
             </h1>
--- containers/Home/Sections/ProblemStatement/index.tsx
+++ containers/Home/Sections/ProblemStatement/index.tsx
@@ -3,7 +3,7 @@ export const ProblemStatementSection = () => (
     <div className='max-w-screen-lg mx-auto py-16 '>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex py-8 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 px-5 pr-16  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="lg:flex-grow md:w-1/2 px-5 pr-5 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 pb-8   text-gray-900 font-montserrat font-bold">
               Technology should work for you
             </h1>
